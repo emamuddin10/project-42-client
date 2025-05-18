8d68c92ec5a15f9b16d93c40a0ae9ce976783a1f
Emam Uddin <emamskingdom@gmail.com>
2025-05-18 16:31:35 +0600

    added site layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from './Routes/router.jsx';
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Routes/router.jsx";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+import { ToastContainer } from 'react-toastify';
+const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <ToastContainer></ToastContainer>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
