e7d22aff8ea40026fdca668d4bfd1790a0a65126
Emam Uddin <emamskingdom@gmail.com>
2025-05-18 22:48:06 +0600

    added user bio data

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -104,8 +104,8 @@
 
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-import { app } from "../firebase/firebase.config";
 import axios from "axios";
+import { app } from "../firebase/firebase.config";
 
 
 
